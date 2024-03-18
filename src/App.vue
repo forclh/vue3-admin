1381d1c18b555fa34dd5072e819c9c5438876fdc
AidenKao <forclh@qq.com>
2024-03-18 10:07:32 +0800

    提交代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>我是App</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = "Aiden";
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
